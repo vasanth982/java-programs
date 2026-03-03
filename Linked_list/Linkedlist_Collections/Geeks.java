ca946d338008d0ce37905e920cce5652fa2cf555
vasanth982 <vasanthvasireddi@gmail.com>
2026-03-03 11:26:12 +0530

    collections

---
+++ Linked_list/Linkedlist_Collections/Geeks.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+
+public class Geeks {
+
+    public static void main(String[] args){
+        
+        // Creating a LinkedList
+        LinkedList<String> l = new LinkedList<String>();
+
+        // Adding elements to the LinkedList using add() method
+        l.add("One");
+        l.add("Two");
+        l.add("Three");
+        l.add("Four");
+        l.add("Five");
+
+        System.out.println(l);
+    }
+}
---
+++ Linked_list/Linkedlist_Collections/Geeks1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Geeks1 {
+
+    public static void main(String args[])
+    {
+        LinkedList<String> ll = new LinkedList<>();
+
+        ll.add("Geeks");
+        ll.add("Geeks");
+        ll.add(1, "For");
+
+        System.out.println(ll);
+    }
+}
