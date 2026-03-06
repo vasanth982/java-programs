6d7f202a36ff0b8d01231d19fb8350d7af73fa14
vasanth982 <vasanthvasireddi@gmail.com>
2026-03-06 21:47:04 +0530

    linkedlist

--- Linked_list/single_Linkedlist/LinkedList6.java
+++ Linked_list/single_Linkedlist/LinkedList6.java
@@ -1,13 +1,13 @@
 import java.util.Scanner;
 
 class Node{
-    int data;
-    Node next;
+int data;
+Node next;
 
-    Node(int data,Node next){
-        this.data = data;
-        this.next = next;
-    }
+Node(int data,Node next){
+this.data = data;
+this.next = next;
+}
 }
 
 class LinkedList6{
@@ -26,39 +26,37 @@ int data = 0;
 
 the_loop:while(ch!=5){
 
-System.out.println("\n1.create 2.insert 3.delete 4.display 5.exit");
+System.out.println("\n1.create 2.insert 3.deleteLast 4.display 5.exit");
 ch = scan.nextInt();
 
 switch(ch){
 
 case 1:
-       System.out.println("Enter a value");
-       data = scan.nextInt();
-       head = l1.createLL(data);
-       break;
+System.out.println("Enter a value");
+data = scan.nextInt();
+head = l1.createLL(data);
+break;
 
 case 2:
-       System.out.println("Enter a value");
-       data = scan.nextInt();
-       l1.insert(head,data);
-       break;
+System.out.println("Enter a value");
+data = scan.nextInt();
+l1.insert(head,data);
+break;
 
 case 3:
-       System.out.println("Enter value to delete");
-       data = scan.nextInt();
-       head = l1.delete(head,data);
-       break;
+head = l1.deleteLast(head);
+break;
 
 case 4:
-       l1.display(head);
-       break;
+l1.display(head);
+break;
 
 case 5:
-       break the_loop;
+break the_loop;
 
 default:
-       System.out.println("Invalid choice");
-       break;
+System.out.println("Invalid choice");
+break;
 }
 }
 }
@@ -95,27 +93,24 @@ current = current.next;
 current.next = temp;
 }
 
-Node delete(Node head,int data){
+Node deleteLast(Node head){
 
-Node temp = head;
-Node prev = null;
-
-while(temp!=null && temp.data!=data){
-prev = temp;
-temp = temp.next;
-}
-
-if(temp==null){
-System.out.println("Value not found");
+if(head==null){
+System.out.println("List is empty");
 return head;
 }
 
-if(prev==null){
-head = temp.next;
+if(head.next==null){
+return null;
 }
-else{
-prev.next = temp.next;
+
+Node temp = head;
+
+while(temp.next.next!=null){
+temp = temp.next;
 }
+
+temp.next = null;
 
 return head;
 }
