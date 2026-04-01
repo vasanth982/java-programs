871329c0e2a757546fa7aa76a631d73e8e5e7256
vasanth982 <vasanthvasireddi@gmail.com>
2026-04-01 11:33:16 +0530

    Trees

---
+++ Trees/CountNodeBFSIterative.java
@@ -0,0 +1,40 @@
+import java.util.Queue;
+import java.util.ArrayDeque;
+class Node {
+int data;
+Node left, right;
+public Node(int data){
+this.data = data;
+this.left = this.right = null;
+}
+}
+class CountNodeBFSIterative {
+Node root;
+int countNodes(Node root) {
+if (root == null)
+return 0;
+Queue<Node> queue = new ArrayDeque<>();
+queue.add(root);
+int count=0;
+while(!queue.isEmpty()) {
+Node curr = queue.poll();
+count ++;
+if(curr.left!=null)
+queue.add(curr.left);
+if(curr.right!=null)
+queue.add(curr.right);
+}
+return count;
+}
+public static void main(String[] args) {
+CountNodeBFSIterative tree = new CountNodeBFSIterative();
+tree.root=new Node(10);
+tree.root.left=new Node(20);
+tree.root.right=new Node(30);
+tree.root.left.left=new Node(40);
+tree.root.left.right=new Node(50);
+tree.root.right.right=new Node(70);
+int count=tree.countNodes(tree.root);
+System.out.println("Total Nodes: "+count);
+}
+}
